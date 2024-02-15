ec7cd773bb0dc04061330ef54c1e5e512143894b
elliesch <ellianna.schwab@gmail.com>
2024-02-15 21:16:13 +0000

    initialized script files

--- flipnslide/__init__.py
+++ flipnslide/__init__.py
@@ -2,20 +2,20 @@
 flipnslide.
 
 This is initialized with the following modules:
-    - .pre         : Preprocess large image data for tiling
-    - .tiling      : Overlap tiling, with specific non-redundant transforms added in
-    - .post        : Postprocess tiles into PyTorch or Tensorflow datasets
+    - .preproc     : Preprocess large image data for tiling
+    - .flipnslide  : Overlap tiling, with specific non-redundant transforms added in
+    - .postproc    : Postprocess tiles into PyTorch or Tensorflow datasets
     - .viz         : Visualization tools for checking on data
     - .util        : Package utilities
 '''
 
 __version__ = '0.0.1'
-__all__ = ['pre', 'tiling', 'post', 'viz', 'util']
+__all__ = ['preproc', 'flipnslide', 'postproc', 'viz', 'util']
 __author__ = 'elliesch <ellianna@berkeley.edu>'
 __minimum_python_version__ = '3.7'
 
-from .pre import *
-from .tiling import *
-from .post import *
+from .preproc import *
+from .flipnslide import *
+from .postproc import *
 from .viz import *
 from .util import *
