87961136394b266a8ccb1019efe426ff1f08fd04
elliesch <ellianna.schwab@gmail.com>
2024-02-16 22:53:02 +0000

    helper function for preprocessing image

--- flipnslide/util.py
+++ flipnslide/util.py
@@ -2,7 +2,6 @@
 
 import pystac_client
 from pystac.extensions.projection import ProjectionExtension as proj
-# import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -12,14 +11,12 @@ import rasterio.features
 import stackstac
 import xarray as xr
 import geopandas as gpd
+from scipy.interpolate import NearestNDInterpolator
 
 
 
-def download_image(coords, time_range, 
-                   bands:list=['blue', 'green', 'red', 'nir08'], 
-                   cat_name:str='landsat-c2-l2',
-                   cloud_cov:int=5,
-                   **kwargs):
+def download_image(coords, time_range, bands, cat_name,
+                   cloud_cov, res, **kwargs):
     '''
     Downloads image cube from planetary computer for a given
     set of decimal degree coordinates across a given time frame 
@@ -38,7 +35,7 @@ def download_image(coords, time_range,
     
     #Define image area to download
     south_bound = coords[0]
-    north_boung = coords[1]
+    north_bound = coords[1]
     east_bound = coords[2]
     west_bound = coords[3]
     
@@ -49,22 +46,18 @@ def download_image(coords, time_range,
                 #longitude,  latitude
                 [west_bound, south_bound],
                 [east_bound, south_bound],
-                [east_bound, north_boung],
-                [west_bound, north_boung],
+                [east_bound, north_bound],
+                [west_bound, north_bound],
                 [west_bound, south_bound],
             ]
         ],
     }
 
     bounds_latlon = rasterio.features.bounds(area_of_interest)
-    
-    #Define other image constraints
-    time_range = "2015-08-15/2015-12-31"
-    bbox = bounds_latlon
 
-    #Fill catalog instance
-    search = catalog.search(collections=[cat_name], 
-                            bbox=bbox, 
+    #Find items in Planetary Computer within search parameters
+    search = catalog.search(collections=cat_name, 
+                            bbox=bounds_latlon, 
                             datetime=time_range, 
                             query={'eo:cloud_cover': {'lt': cloud_cov}
                                   })
@@ -78,13 +71,46 @@ def download_image(coords, time_range,
     stack = stackstac.stack(items, epsg=lc_epsg, 
                             assets=bands,
                             bounds_latlon=bounds_latlon, 
-                            resolution=30)
+                            resolution=res)
     
     monthly = stack.resample(time="MS").median("time", keep_attrs=True)
     
     merged = stackstac.mosaic(stack, dim="time", axis=None).squeeze().compute()
     
-    #Save as an xr dataset object
+    #Move to a numpy array with correct dimensions
     merged_set = merged.to_dataset(dim='band')
+    image_data = merged_set.to_array(dim='band').data
     
-    return merged_set
+    return image_data
+
+
+def preprocess(data, **kwargs):
+    '''
+    Preprocesses image cube to prepare for use with ML algorithms.
+    
+    Fills in any nans band-by-band using scipy interpolation. 
+    Then standardizes the data cube, saving the cleaned data.
+    '''
+    
+    #Throw out time frames with too many nans
+    ###!!!>>> HAVE TO FIGURE OUT WHAT DIMS WOULD BE TIME <<<!!!###
+    
+    #Fills any nans using scipy interpolate 
+    ###!!!>>> DOUBLE CHECK THAT THIS SHAPE USING CORRECT DIM <<<!!!###
+    band_count = data.shape[0]
+    
+    for band in range(band_count):
+        interp_mask=np.where(~np.isnan(data[band]))
+        interp = NearestNDInterpolator(np.transpose(interp_mask), data[band][interp_mask])
+        data[band] = interp(*np.indices(data[band].shape))
+        
+    #!> A good test here is to check that there aren't any nans <!#
+        
+    #Shift and scale the data cube, band-by-band
+    ###!!!>>> DOUBLE CHECK THAT BAND_COUNT USING CORRECT DIM <<<!!!###
+    normed_data = np.zeros(data.shape)
+
+    for band in range(band_count):
+        normed_data[band] = ((data[band] - np.mean(data[band]))/np.std(data[band]))
+        
+    return normed_data
