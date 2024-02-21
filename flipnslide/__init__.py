3be2bc09f7385ddc3224ac0287b047b4cc893f04
elliesch <ellianna.schwab@gmail.com>
2024-02-21 22:22:14 +0000

    init updates

--- flipnslide/__init__.py
+++ flipnslide/__init__.py
@@ -2,20 +2,20 @@
 flipnslide.
 
 This is initialized with the following modules:
-    - .preproc     : Preprocess large image data for tiling
-    - .flipnslide  : Overlap tiling, with specific non-redundant transforms added in
-    - .postproc    : Postprocess tiles into PyTorch or Tensorflow datasets
+    - .ingest      : Import large data image in location and preprocess
+    - .tiling      : Overlap tiling, with specific non-redundant transforms added in
+    - .dataset     : Postprocess tiles into PyTorch or Tensorflow datasets
     - .viz         : Visualization tools for checking on data
     - .util        : Package utilities
 '''
 
 __version__ = '0.0.1'
-__all__ = ['preproc', 'flipnslide', 'postproc', 'viz', 'util']
+__all__ = ['ingest', 'tiling', 'dataset', 'viz', 'util']
 __author__ = 'elliesch <ellianna@berkeley.edu>'
 __minimum_python_version__ = '3.7'
 
-from .preproc import *
-from .flipnslide import *
-from .postproc import *
+from .ingest import *
+from .tiling import *
+from .dataset import *
 from .viz import *
 from .util import *
