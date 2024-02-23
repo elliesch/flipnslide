c3872450ece60edf1629030b040e1731a5e12d59
elliesch <ellianna.schwab@gmail.com>
2024-02-23 23:40:15 +0000

    added viz functionality

--- flipnslide/ingest.py
+++ flipnslide/ingest.py
@@ -1,24 +1,64 @@
-''' Ingest Images (right now only optimized for downloading COGs) '''
+''' Ingest Images from Planetary Computer'''
 
 from .util import download_image, preprocess
+from .viz import ingest_viz
+
 
 
 class ImageIngest:
     
-    def __init__(self, coords, time_range, 
-                 bands:list=['blue', 'green', 'red', 'nir08'], 
-                 cat_name:list=['landsat-c2-l2'],
-                 cloud_cov:int=5,
-                 res:int=30, **kwargs):
+    def __init__(self, coords, time_range, viz:bool=False, **kwargs):
+            '''
+        Initialize ImageIngest to download and preprocess an image.
+
+        Parameters:
+        - coords (List[float]): List of four floats indicating corners of the requested image 
+          in long/lat coordinates. Should follow this format: 
+          [southern_boundary, northern_boundary, eastern_boundary, western_boundary].
+        - time_range (str): String indicating the time range for the requested image. 
+          Should follow this format: 'YYYY-MM-DD/YYYY-MM-DD'.
+        - viz (bool): Flag indicating whether to visualize downloaded image.
+          
+        Optional Parameters passed from **kwargs:
+        - bands (List[str]): List indicating bands of the requested image 
+          (default is ['blue', 'green', 'red', 'nir08']).
+        - res (int): Integer representing the resolution of the requested image. Should match the 
+          resolution of the data catalog (default is 30).
+
+        Attributes:
+        - coords (List[float]): List of four floats indicating corners of the requested image in 
+          long/lat coordinates.
+        - time_range (str): String indicating the time range for the requested image.
+        - image (numpy.ndarray): NumPy ndarray representing the preprocessed image.
+        
+        Optional Attributes:
+        - bands (List[str]): List indicating bands of the requested image.
+        - res (int): Integer representing the resolution of the requested image.
+
+        Notes:
+        - The ImageIngest class is used to download and preprocess an image based on the provided 
+          coordinates, time range, and optional parameters.
+        - The downloaded image is preprocessed using the preprocess function before being stored in 
+          the 'image' attribute.
+        '''
         
         #Meta data
         self.coords = coords
         self.time_range = time_range
-        self.bands = bands
-        self.res = res
+        
+        #Optional meta data
+        if 'bands' in kwargs:
+            self.bands = kwargs['bands']
+            
+        if 'res' in kwargs:
+            self.res = kwargs['res']
         
         #Import image and preprocess
-        raw_image = download_image(coords, time_range, bands, 
-                                   cat_name, cloud_cov, res)
+        raw_image = download_image(coords, time_range, **kwargs)
         
         self.image = preprocess(raw_image)
+        
+        #Visualize the preprocessed image
+        if viz == True:
+            ingest_viz(self.image)
+            
